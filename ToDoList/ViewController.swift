df4bcaeead20b127a2b0da8b858b21f79782d3fd
Ricardo Roman Landeros <ricardomon85@gmail.com>
2025-01-01 15:29:56 -0600

    subir a github

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor.lightGray
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        self.title = "ToDo List"
     }
 
 
